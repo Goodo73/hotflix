ce8f4be6d5911b38ed4f5b5c5a86617fb1f12a60
Mike Goodwin <goodo73@gmail.com>
2015-05-25 21:15:43 +1000

    Added admin index page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,10 @@
 class AdminController < ApplicationController
   # before_action :require_admin
 
+  def index
+  	
+  end
+
   # def require_admin
   #   unless current_user.admin?
   #     redirect_to root_path
